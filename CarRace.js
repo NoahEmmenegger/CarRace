5581db1b024c52201e4b20a821b7287ed761881b
Noah Emmenegger <noahemmenegger.business@gmail.com>
2020-04-08 10:16:52 +0200

    add cars

--- CarRace.js
+++ CarRace.js
@@ -6,6 +6,8 @@ class CarRace extends Phaser.Scene {
     preload() {
         this.load.image('land', './assets/land.png')
         this.load.image('street', './assets/street.png')
+        this.load.image('car_1', './assets/car_1.png')
+        this.load.image('car_2', './assets/car_2.png')
     }
 
     create() {
@@ -14,6 +16,12 @@ class CarRace extends Phaser.Scene {
 
         this.street = this.add.image(480, 270, 'street')
         this.street.setScale(0.5)
+
+        this.car_1 = this.add.image(200, 300, 'car_1')
+        this.car_1.setScale(0.5)
+
+        this.car_2 = this.add.image(300, 300, 'car_2')
+        this.car_2.setScale(0.5)
     }
 
     update(delta) {
