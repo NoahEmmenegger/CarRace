fe55920ca19d7f3772c0c7ebd31ca56cd6d3db90
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-05-05 17:43:51 +0200

    Auto Fahrfunktion hinzugefügt

--- CarRace.js
+++ CarRace.js
@@ -8,16 +8,23 @@ class CarRace extends Phaser.Scene {
         this.load.image('street', './assets/street.png')
         this.load.image('car_1', './assets/car_1.png')
         this.load.image('car_2', './assets/car_2.png')
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.wasdCursors = this.input.keyboard.addKeys('W,S,A,D');
     }
 
     create() {
+        this.carSpeed = 150;
+        this.speedCar_1 = 0;
+        this.speedCar_2 = 0;
+        this.staticFriction = 1;
+
         this.land = this.physics.add.sprite(480, 270, 'land');
         this.land.setScale(0.5)
 
         this.street = this.add.image(480, 270, 'street')
         this.street.setScale(0.5)
 
-        this.car_1 = this.physics.add.sprite(200, 300, 'car_1')
+        this.car_1 = this.physics.add.image(200, 300, 'car_1');
         this.car_1.setScale(0.5)
         var car_1Physic = this.physics.add.overlap(this.land, this.car_1, (land, car) => this.checkCollision(land, car, car_1Physic), null, this);
 
@@ -28,7 +35,52 @@ class CarRace extends Phaser.Scene {
     }
 
     update(delta) {
-        
+        this.CarDriveCheck();
+    }
+
+    CarDriveCheck()
+    {
+        if (this.cursors.left.isDown) {
+            this.car_1.rotation -= 0.1;
+        }   
+        if (this.cursors.right.isDown) {
+            this.car_1.rotation += 0.1;
+        }
+
+        if(this.cursors.up.isDown)
+        {
+            this.speedCar_1 = this.carSpeed; 
+        }
+        else if(this.cursors.down.isDown){
+            this.speedCar_1 = -this.carSpeed;
+        }
+        else{
+            this.speedCar_1 = 0;
+        }
+
+        if (this.wasdCursors.A.isDown) {
+            this.car_2.rotation -= 0.1;
+        }   
+        if (this.wasdCursors.D.isDown) {
+            this.car_2.rotation += 0.1;
+        }
+
+        if(this.wasdCursors.W.isDown)
+        {
+            this.speedCar_2 = this.carSpeed; 
+        }
+        else if(this.wasdCursors.S.isDown){
+            this.speedCar_2 = -this.carSpeed;
+        }
+        else{
+            this.speedCar_2 = 0;
+        }
+
+        this.car_1.setVelocityX(Math.sin(this.car_1.rotation)*this.speedCar_1);
+        this.car_1.setVelocityY(-Math.cos(this.car_1.rotation)*this.speedCar_1);
+
+        this.car_2.setVelocityX(Math.sin(this.car_2.rotation)*this.speedCar_2);
+        this.car_2.setVelocityY(-Math.cos(this.car_2.rotation)*this.speedCar_2);
     }
 
 
